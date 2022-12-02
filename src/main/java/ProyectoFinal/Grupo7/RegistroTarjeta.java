0eeeb931907c10ecfc71cbb191aa7d94be379c1a
earias898 <esteban.ariasorozco32@gmail.com>
2022-12-01 18:08:31 -0600

    proyecto

---
+++ src/main/java/ProyectoFinal/Grupo7/RegistroTarjeta.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProyectoFinal.Grupo7;
+
+/**
+ *
+ * @author esteb
+ */
+public class RegistroTarjeta {
+  var cc_number_saved = "";
+function validar(input){
+  var elemento = document.getElementById("cardNumber").value;
+  if (/(4[0-9]{12}(?:[0-9]{3})?)/.test(elemento)) {
+    document.getElementById("cardlogo").classList.add("fa-cc-visa");
+    document.getElementById("cardLogoTop").innerHTML = "<img class='img-responsive pull-right' src='https://i.imgur.com/iqIDYfz.png'>"
+  }
+  if (/3[47][0-9]{13}/.test(elemento)) {
+    document.getElementById("cardlogo").classList.add("fa-cc-amex");
+    document.getElementById("cardLogoTop").innerHTML = "<img class='img-responsive pull-right' src='https://i.imgur.com/WluzPvZ.png'>"
+  }
+  if (/5[1-5][0-9]{14}/.test(elemento)) {
+    document.getElementById("cardlogo").classList.add("fa-cc-mastercard");
+    document.getElementById("cardLogoTop").innerHTML = "<img class='img-responsive pull-right' src='https://i.imgur.com/1U8OBnM.png'>"
+  }
+  if (/6(?:011|5[0-9]{2})[0-9]{12}/.test(elemento)) {
+    document.getElementById("cardlogo").classList.add("fa-cc-discover");
+    document.getElementById("cardLogoTop").innerHTML = "<img class='img-responsive pull-right' src='https://i.imgur.com/H5lJRwk.png'>"
+  }
+  if(elemento == 0){
+    document.getElementById("cardlogo").classList.remove("fa-cc-visa");
+    document.getElementById("cardLogoTop").innerHTML = "<img class='img-responsive pull-right' src='https://i.imgur.com/gIMFDbp.png'>"
+document.getElementById("cardlogo").classList.remove("fa-cc-amex");
+document.getElementById("cardlogo").classList.remove("fa-cc-mastercard");
+document.getElementById("cardlogo").classList.remove("fa-cc-discover");
+  }
+  // Luhn Algorithm
+	var sum = 0;
+  var numdigits = input.length;
+  var parity = numdigits % 2;
+  for(var i=0; i < numdigits; i++) {
+    var digit = parseInt(input.charAt(i))
+            if(i % 2 == parity) digit *= 2;
+    if(digit > 9) digit -= 9;
+    sum += digit;
+  }
+  return (sum % 10) == 0;
+}
+}
